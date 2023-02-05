f92d6c15d75653c1d062f0497243aaa1c7b3bce7
ElenPolyanska <evp387@gmail.com>
2023-02-05 22:27:43 +0200

    1st task

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,28 @@
+const startEl = document.querySelector('button[data-start]');
+const stopEl = document.querySelector('button[data-stop]');
+const bodyEl = document.querySelector('body');
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+startEl.addEventListener('click', startChangeHandler);
+stopEl.addEventListener('click', stopChangeHandler);
+
+let timerId = null;
+function startChangeHandler() {
+  bodyEl.style.backgroundColor = `${getRandomHexColor()}`;
+  timerId = setInterval(() => {
+    bodyEl.style.backgroundColor = `${getRandomHexColor()}`;
+  }, 1000);
+  startEl.disabled = true;
+  if (stopEl.hasAttribute('disabled')) {
+    stopEl.disabled = false;
+  }
+}
+
+function stopChangeHandler() {
+  clearInterval(timerId);
+  stopEl.disabled = true;
+  startEl.disabled = false;
+}
